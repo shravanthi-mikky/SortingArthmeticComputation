a5897a4852167cd86055d39bdb57084112e7a8af
shravanthi-mikky <shravanthi27041996@gmail.com>
2022-01-07 21:12:07 +0530

    UC4 Comutation is done

--- sortArthmetic.sh
+++ sortArthmetic.sh
@@ -1,5 +1,5 @@
 #! /bin/bash -x
-echo " Sorting Arthmetic Computation Probelm "
+echo "Sorting Arthmetic Computation Probelm "
 read -p "Enter value of a : " a
 read -p "Enter value of b : " b
 read -p " Enter value of c : " c
